df5486281cedc0790666e65a952e46089e10f45e
Tlaster <tlaster@outlook.com>
2025-08-07 17:11:02 +0900

    add macos support

--- api-gen-runtime/build.gradle.kts
+++ api-gen-runtime/build.gradle.kts
@@ -21,7 +21,7 @@ kotlin {
         implementation(kotlin("reflect"))
       }
     }
-    val iosMain by getting {
+    val appleMain by getting {
       dependencies {
         implementation(libs.ktor.darwin)
       }

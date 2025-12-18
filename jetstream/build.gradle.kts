318aaf640a2c85f59ab0ca8cb34492f6a995a1ce
Tlaster <tlaster@outlook.com>
2025-12-18 13:18:50 +0900

    0.3.4

--- jetstream/build.gradle.kts
+++ jetstream/build.gradle.kts
@@ -52,3 +52,6 @@ tasks.apiCheck.configure { dependsOn(generateLexicons) }
 tasks.withType<DokkaBaseTask>().configureEach {
   dependsOn(tasks.withType<KotlinCompile>())
 }
+tasks.named("dokkaGeneratePublicationHtml") {
+    dependsOn(tasks.named("generateLexicons"))
+}
